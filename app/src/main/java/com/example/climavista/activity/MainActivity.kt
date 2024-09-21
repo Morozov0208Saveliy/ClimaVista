1a93ab9e439e8eef7884e7732380d347da187490
Morozov0208Saveliy <savelmor@yandex.ru>
2024-09-21 14:04:55 +0200

    Fragment&&navigation 2

--- app/src/main/java/com/example/climavista/activity/MainActivity.kt
+++ app/src/main/java/com/example/climavista/activity/MainActivity.kt
@@ -7,6 +7,10 @@ import com.example.climavista.R
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            setContentView(R.layout.activity_main)
+
+        }
     }
 }
